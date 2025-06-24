c5bb1cf0945e436115dfa86805636ba2d14bbae0
Jon Rothbaum <jon.rothbaum@gmail.com>
2025-06-23 22:56:08 -0400

    Remove fpic from conan windows builds

--- crates/cpp-sas7bdat/vendor/conan_files/shared.py
+++ crates/cpp-sas7bdat/vendor/conan_files/shared.py
@@ -5,8 +5,8 @@ class SharedConfig:
     def apply_options(conanfile):
         """Apply shared build options to any conanfile"""
         conanfile.options["*"].shared = False
-        conanfile.options["*"].fPIC = True
-        
+        if conanfile.settings.get_safe("os") != "Windows":
+            conanfile.options["*"].fPIC = True
         # Common boost settings
         if "boost" in str(conanfile.requires):
             conanfile.options["boost"].without_python = True
